a148208a9292bd1a38fb5aa2df5f63f5e092f089
Ed Reed <reed.ej@hotmail.com>
2022-10-19 14:08:45 -0700

    Add odometry

--- src/main/java/frc/robot/subsystems/RomiDrivetrain.java
+++ src/main/java/frc/robot/subsystems/RomiDrivetrain.java
@@ -4,36 +4,47 @@
 
 package frc.robot.subsystems;
 
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.kinematics.DifferentialDriveOdometry;
 import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.interfaces.Gyro;
 import edu.wpi.first.wpilibj.motorcontrol.Spark;
+import edu.wpi.first.wpilibj.romi.RomiGyro;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants.DigitalInputPort;
 import frc.robot.Constants.PWMPort;
 
 public class RomiDrivetrain extends SubsystemBase {
   private static final double kCountsPerRevolution = 1440.0;
-  private static final double kWheelDiameterInch = 2.75591; // 70 mm
+  private static final double kWheelDiameterMeters = 0.070; // 70 mm
 
-  // The Romi has the left and right motors set to
-  // PWM channels 0 and 1 respectively
+  // Set up the left and right motors.
   private final Spark m_leftMotor = new Spark(PWMPort.LeftMotor.get());
   private final Spark m_rightMotor = new Spark(PWMPort.RightMotor.get());
-
-  // The Romi has onboard encoders that are hardcoded
-  // to use DIO pins 4/5 and 6/7 for the left and right
-  private final Encoder m_leftEncoder = new Encoder(DigitalInputPort.LeftEncoderA.get(), DigitalInputPort.LeftEncoderB.get());
-  private final Encoder m_rightEncoder = new Encoder(DigitalInputPort.RightEncoderA.get(), DigitalInputPort.RightEncoderB.get());
 
   // Set up the differential drive controller
   private final DifferentialDrive m_diffDrive = new DifferentialDrive(m_leftMotor, m_rightMotor);
 
+  // Set up the encoders and gyro.
+  private final Encoder m_leftEncoder = new Encoder(
+      DigitalInputPort.LeftEncoderA.get(),
+      DigitalInputPort.LeftEncoderB.get());
+  private final Encoder m_rightEncoder = new Encoder(
+      DigitalInputPort.RightEncoderA.get(),
+      DigitalInputPort.RightEncoderB.get());
+  private final Gyro m_gyro = new RomiGyro();
+
+  // Set up the differential drive odometry
+  private final DifferentialDriveOdometry m_odometry = new DifferentialDriveOdometry(m_gyro.getRotation2d());
+
   /** Creates a new RomiDrivetrain. */
   public RomiDrivetrain() {
-    // Use inches as unit for encoder distances
-    m_leftEncoder.setDistancePerPulse((Math.PI * kWheelDiameterInch) / kCountsPerRevolution);
-    m_rightEncoder.setDistancePerPulse((Math.PI * kWheelDiameterInch) / kCountsPerRevolution);
-    resetEncoders();
+    // Use meters as unit for encoder distances
+    m_leftEncoder.setDistancePerPulse((Math.PI * kWheelDiameterMeters) / kCountsPerRevolution);
+    m_rightEncoder.setDistancePerPulse((Math.PI * kWheelDiameterMeters) / kCountsPerRevolution);
+    resetPosition();
 
     // Invert right side since motor is flipped
     m_rightMotor.setInverted(true);
@@ -47,22 +58,71 @@ public class RomiDrivetrain extends SubsystemBase {
     m_diffDrive.stopMotor();
   }
 
-  public void resetEncoders() {
-    m_leftEncoder.reset();
-    m_rightEncoder.reset();
+  /** Resets the initial position to the origin with a heading of 0°. */
+  public void resetPosition() {
+    resetPosition(new Pose2d());
   }
 
-  public double getLeftDistanceInch() {
+  /**
+   * Resets the initial position to the specified pose.
+   * 
+   * @param initialPose The initial position of the robot.
+   */
+  public void resetPosition(Pose2d initialPose) {
+    m_leftEncoder.reset();
+    m_rightEncoder.reset();
+    m_gyro.reset();
+
+    Rotation2d gyroOffset = m_gyro.getRotation2d();
+
+    m_odometry.resetPosition(initialPose, gyroOffset);
+  }
+
+  /**
+   * Returns the heading of the robot.
+   * 
+   * @return The heading of the robot in degress using the NWU axis convention.
+   *         An increasing value indicates rotation in the counter-clockwise
+   *         direction.
+   */
+  public Rotation2d getHeading() {
+    return m_odometry.getPoseMeters().getRotation();
+  }
+
+  /**
+   * Returns the distance travelled by the left wheel in meters.
+   * 
+   * @return The distance travelled by the left wheel in meters.
+   */
+  public double getLeftDistanceMeters() {
     return m_leftEncoder.getDistance();
   }
 
-  public double getRightDistanceInch() {
+  /**
+   * Returns the distance travelled by the right wheel in meters.
+   * 
+   * @return The distance travelled by the right wheel in meters.
+   */
+  public double getRightDistanceMeters() {
     return m_rightEncoder.getDistance();
+  }
+
+  /**
+   * Returns the current pose of the robot.
+   * 
+   * @return The current pose of the robot.
+   */
+  public Pose2d getPose() {
+    return m_odometry.getPoseMeters();
   }
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+    Rotation2d heading = m_gyro.getRotation2d();
+    double leftDistance = m_leftEncoder.getDistance();
+    double rightDistance = m_rightEncoder.getDistance();
+
+    m_odometry.update(heading, leftDistance, rightDistance);
   }
 
   @Override
