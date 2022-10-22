7a322e55f0c525cffd06d0d6446a27853c3ba669
Ed Reed <reed.ej@hotmail.com>
2022-10-22 15:06:55 -0700

    Add odometry (#3)
    * Add odometry
    
    * Add Romi Shuffleboard tab
    
    * Remove SysId file

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,12 +4,21 @@
 
 package frc.robot;
 
+import edu.wpi.first.util.sendable.Sendable;
+import edu.wpi.first.util.sendable.SendableBuilder;
+import edu.wpi.first.wpilibj.RobotController;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInLayouts;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInWidgets;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardLayout;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.commands.DriveUsingXboxController;
 import frc.robot.subsystems.RSL;
 import frc.robot.subsystems.RomiDrivetrain;
+import java.util.Map;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -34,6 +43,8 @@ public class RobotContainer {
     configureButtonBindings();
 
     m_romiDrivetrain.setDefaultCommand(new DriveUsingXboxController(m_romiDrivetrain, m_xboxController));
+
+    initShuffleboard();
   }
 
   /**
@@ -52,5 +63,31 @@ public class RobotContainer {
   public Command getAutonomousCommand() {
     // An ExampleCommand will run in autonomous
     return m_autoCommand;
+  }
+
+  /** Initializes the Romi Shuffleboard tab. */
+  private void initShuffleboard() {
+    ShuffleboardTab romiTab = Shuffleboard.getTab("Romi");
+
+    ShuffleboardLayout odometryLayout = romiTab.getLayout("Odometry", BuiltInLayouts.kList).
+      withPosition(0, 0).
+      withSize(2, 3);
+
+    odometryLayout.addNumber("Y", () -> m_romiDrivetrain.getPose().getY());
+    odometryLayout.addNumber("X", () -> m_romiDrivetrain.getPose().getX());
+    odometryLayout.add("Heading", new Sendable() {
+      public void initSendable(SendableBuilder builder) {
+        builder.setSmartDashboardType("Gyro");
+        builder.addDoubleProperty("Value", () -> m_romiDrivetrain.getHeading().getDegrees(), null);
+      }
+    });
+
+    ShuffleboardLayout statusLayout = romiTab.getLayout("Status", BuiltInLayouts.kList).
+      withPosition(2, 0).
+      withSize(2, 3);
+    
+    statusLayout.addNumber("Battery", () -> RobotController.getBatteryVoltage()).
+      withWidget(BuiltInWidgets.kVoltageView).
+      withProperties(Map.of("Max", 7.2));
   }
 }
