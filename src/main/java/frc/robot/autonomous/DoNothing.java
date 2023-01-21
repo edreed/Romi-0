3774f84ee67cf021fc405c2b51efe66c04756adc
Ed Reed <reed.ej@hotmail.com>
2023-01-20 22:49:37 -0800

    WIP: Test AutonomousCommandMethod

--- src/main/java/frc/robot/autonomous/DoNothing.java
+++ src/main/java/frc/robot/autonomous/DoNothing.java
@@ -5,14 +5,22 @@
 package frc.robot.autonomous;
 
 import com.nrg948.autonomous.AutonomousCommand;
+import com.nrg948.autonomous.AutonomousCommandMethod;
 
 import edu.wpi.first.wpilibj.DriverStation;
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
 import frc.robot.subsystems.Subsystems;
 
 /** An autonomous command that performs no action. */
 @AutonomousCommand(name = "Do Nothing", isDefault = true)
 public final class DoNothing extends WaitUntilCommand {
+
+    @AutonomousCommandMethod(name = "Do Nothing (method)")
+    public static Command factory(Subsystems subsystems) {
+        return new DoNothing(subsystems);
+    }
+
     public DoNothing(Subsystems subsystems) {
         super(() -> !DriverStation.isAutonomousEnabled());
     }
